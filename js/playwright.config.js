e881d90c2be3e7f37a8548df75a47f8f8a572688
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2026-06-14 14:26:16 -0400

    Update playwright.config.js

--- js/playwright.config.js
+++ js/playwright.config.js
@@ -22,7 +22,6 @@ export default defineConfig({
     },
   ],
   webServer: {
-<<<<<<< before updating
     command: "pnpm run start:tests",
     url: "http://127.0.0.1:3000",
     reuseExistingServer: !process.env.CI,
